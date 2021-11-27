cfaab3dd5489de095a61d29383a8dbdebfa5a267
abordino <U$$9Y~QaO,1;2O-5{2F&(X*[4>
2021-11-27 11:49:41 +0100

    lung model add

---
+++ model/lung/lassoSVM_lung.R
@@ -0,0 +1,33 @@
+# LASSO-SVM 
+library(sparseSVM)
+library(caret)
+library(smotefamily)
+
+# load the dataset
+train = read.csv("../../dataset/lung_training.csv")
+test = read.csv("../../dataset/lung_test.csv")
+
+# balancing the minority class using the SMOTE function 
+length(which(train$label == 1))/nrow(train)
+genData = SMOTE(train[,-1], train$label, dup_size = 6)
+blood.training_balanced = genData$data
+length(which(blood.training_balanced$label == 1))/nrow(blood.training_balanced)
+
+train_X = as.matrix(train[, -c(1, 17395)])
+train_y = train$label
+
+test_X = as.matrix(test[, -c(1,17395)])
+test_y = test$label
+
+# choose the best alpha (ElasticNet) and lambda (for shrikage) with 10-fold-cv
+cv.SVM = cv.sparseSVM(train_X, train_y, seed = 42)
+plot(cv.SVM)
+
+# fit this SVM
+lasso.SVM = sparseSVM(train_X, train_y, lambda = cv.SVM$lambda.min)
+sum(lasso.SVM$weights > 0)
+pred_y = predict(object = lasso.SVM, X = test_X)
+confusionMatrix(factor(pred_y), factor(test_y))
+
+
+
